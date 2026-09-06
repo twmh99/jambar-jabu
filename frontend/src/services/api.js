5ca0ae8b6ec9de5d5594bbe289fc389fc3beb48d
twmh99 <twmh14@gmail.com>
2026-09-06 08:03:57 +0700

    Perbaikan konfigurasi env lokal dan url frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://api.medanjaya.site/api";
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
